02d49db134f42970f3442fada89fca6150063ef6
Alaa Shabouni <alaashabouni@gmail.com>
2018-11-12 17:55:28 -0800

    Able to filter list of coffee shops

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ class App extends Component {
   styles = {
     menuButton: {
       marginLeft: 10,
-      merginRight: 20,
+      marginRight: 20,
       position: "absolute",
       left: 10,
       top: 20,
       background: "white",
-      padding: 10
+      padding: 10,
     },
     hide: {
       display: 'none'
@@ -30,32 +30,53 @@ class App extends Component {
     }
   };
 
+  componentDidMount = () => {
+    this.setState({
+      ...this.state,
+      filtered: this.filterLocations(this.state.all, "")
+    });
+  }
+
   toggleList = () => {
     this.setState({
       open: !this.state.open
     });
   }
 
-  render = () => {
-    return ( <div className="App">
-        <div>
-          <button onClick={this.toggleList} style={this.styles.menuButton}>
-            <i className="fa fa-bars"></i>
-          </button>
-          <h1>Oakland, CA Coffee Shops</h1>
-        </div>
-          <MapDisplay
-            lat={this.state.lat}
-            lon={this.state.lon}
-            zoom={this.state.zoom}
-            locations={this.state.all}/>
-          <ListMenu
-            locations={this.state.all}
-            open={this.state.open}
-            toggleList={this.toggleList}/>
-        </div>
-    );
+  updateQuery = (query) => {
+    this.setState({
+      ...this.state,
+      selectedIndex: null,
+      filtered: this.filterLocations(this.state.all, query)
+    });
   }
+
+  filterLocations = (locations,query) => {
+    return locations.filter(location => location.name.toLowerCase().includes(query.toLowerCase()));
+  }
+
+  render = () => {
+   return (
+     <div className="App">
+       <div>
+         <button onClick={this.toggleList} style={this.styles.menuButton}>
+           <i className="fa fa-bars"></i>
+         </button>
+         <h1>Oakland, CA Coffee Shops</h1>
+       </div>
+       <MapDisplay
+         lat={this.state.lat}
+         lon={this.state.lon}
+         zoom={this.state.zoom}
+         locations={this.state.filtered}/>
+       <ListMenu
+         locations={this.state.filtered}
+         open={this.state.open}
+         toggleList={this.toggleList}
+         filterLocations={this.updateQuery}/>
+     </div>
+   );
+ }
 }
 
 export default App;
